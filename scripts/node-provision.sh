9cccac573e523a86148b7337dd6775aa497b1a97
vinzcraft <vinzcraft@hotmail.com>
2021-01-07 17:28:41 +0100

    update node provision kubernetes in comment

--- scripts/node-provision.sh
+++ scripts/node-provision.sh
@@ -14,13 +14,17 @@ systemctl enable --now docker
 sudo usermod -aG docker vagrant
 
 # Kubernetes
-#swapoff -a
-#sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
-#cp /vagrant/config/ip-forward.conf /etc/sysctl.d/
-#apt-get -y install software-properties-common apt-transport-https curl jq
-#curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
-#apt-add-repository "deb https://apt.kubernetes.io/ kubernetes-xenial main"
-#apt-get -y install kubeadm kubelet kubectl kubernetes-cni
+
+#sudo apt-get update && sudo apt-get install -y apt-transport-https curl jq
+#curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+#cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+#deb https://apt.kubernetes.io/ kubernetes-xenial main
+#EOF
+#sudo apt-get update
+#sudo apt-get install -y kubelet kubeadm kubectl
+#sudo apt-mark hold kubelet kubeadm kubectl
+
+
 
 # Benutzer vagrant zu Gruppen hinzufügen
 #adduser vagrant docker
